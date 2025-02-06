3b5d6c1edae405062d177b3f392bab7cb80ac47c
Mintu Sikder <mintusikder15@gmail.com>
2025-02-06 15:34:16 +0600

    switch

---
+++ switch.js
@@ -0,0 +1,15 @@
+var theme = "blue";
+
+switch (theme) {
+  case "light":
+    console.log("Light Theme");
+    break;
+  case "dark":
+    console.log("dark theme");
+    break;
+  case "blue":
+    console.log("blue theme");
+    break;
+  default:
+    console.log("system theme apply");
+}
