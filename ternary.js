08343629d659917c45699bd623fcbda5d42685b4
Mintu Sikder <mintusikder15@gmail.com>
2025-02-07 19:58:51 +0600

    added ternary

---
+++ ternary.js
@@ -0,0 +1,15 @@
+/**
+ *
+ * Ternary ----> three part
+ *
+ * condition  ? do something when true   :  do something when false
+ *
+ */
+const age = 12;
+age >= 18 ? console.log("vote dio") : console.log("Gumay thako");
+
+
+let price = 100 ;
+const isLeader = false;
+
+price = isLeader === true ? 0 : price + 100;
