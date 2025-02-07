9534b84548966e84a60d6cc541b5d56099b1cbc0
Mintu Sikder <mintusikder15@gmail.com>
2025-02-07 10:13:18 +0600

    added restaurant discount

---
+++ restaurant-discount.js
@@ -0,0 +1,18 @@
+const age = 51 ;
+const price = 500;
+if(age <= 12){
+    console.log("You can eat for free")
+}else if(age <= 60){
+        //50% discount
+        const discount = price * 50 / 100 
+        const payAmount = price - discount 
+        console.log(payAmount)
+}else if(age <= 40){
+    //40% discount
+    const discount = price * 40 / 100 
+    const payAmount = price - discount 
+    console.log(payAmount)
+}
+else{
+    console.log(price)
+}
