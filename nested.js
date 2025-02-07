c4c48d0ff031d37b8954f5ddbd7dc6944b688a6c
Mintu Sikder <mintusikder15@gmail.com>
2025-02-07 10:29:17 +0600

    added nested

---
+++ nested.js
@@ -0,0 +1,18 @@
+const money = 0;
+
+if(money >= 40){
+    console.log("bro you are rich")
+}else{
+    if(money >= 10){
+        console.log("bro you are poor")
+    }
+    else{
+        if(money > 0){
+            console.log("Eat muri")
+        }
+        else{
+            console.log("Tumi join korte parba na")
+        }        
+    }
+  
+}
