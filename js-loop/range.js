4e39d2792fe7df9e0de70bcbac9bcd3f19ef2eba
Mintu Sikder <mintusikder15@gmail.com>
2025-02-08 20:03:43 +0600

    added renge sum

---
+++ js-loop/range.js
@@ -0,0 +1,13 @@
+// let num = 1 
+// while(num <= 10){
+//     console.log(num)
+//     num++
+// }
+
+//----------50 to 100----------
+
+let num = 50;
+while(num <= 100){
+    console.log(num)
+    num++
+}
