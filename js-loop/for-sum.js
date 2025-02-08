6b97e79f689646d328615543a6a1370dc9b7f71a
Mintu Sikder <mintusikder15@gmail.com>
2025-02-08 20:58:58 +0600

    added for loop

---
+++ js-loop/for-sum.js
@@ -0,0 +1,7 @@
+let sum = 0;
+for(let i =200 ; i<=500; i++){
+    sum = sum + i
+    // console.log(i)
+
+}
+console.log(sum)
---
+++ js-loop/odd-even.js
@@ -0,0 +1,6 @@
+for(let i=1; i<=20; i=i+2){
+    console.log(i)
+}
+for(let i=0; i<=20; i=i+2){
+    console.log(i)
+}
