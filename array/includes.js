bc21f18fe7608e518d1cde4ab1f4d0ac63c26199
Mintu Sikder <mintusikder15@gmail.com>
2025-02-08 10:34:18 +0600

    added includes

---
+++ array/includes.js
@@ -0,0 +1,10 @@
+const friends = ["balam","colom","robin","mintu","sajib"]
+
+console.log(friends.includes("mintu"))
+console.log(friends.includes("fmintu"))
+
+if(friends.includes("fmintu")){
+    console.log("party")
+}else{
+    console.log("No food no eat")
+}
