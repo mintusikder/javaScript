6e6deb7b568ffcac2bc3cae4e3340aeca88c0fd0
Mintu Sikder <mintusikder15@gmail.com>
2025-02-08 19:43:26 +0600

    added loop

---
+++ js-loop/for-of.js
@@ -0,0 +1,13 @@
+const numbers = [12, 24, 5, 6, 88, 45];
+
+console.log(numbers);
+
+for(const num of numbers){
+    console.log(num)
+}
+
+const fruits = ["banana", "orange","mango","apple"]
+
+for(const fruit of fruits){
+    console.log(fruit)
+}
---
+++ js-tasks/free-drinks.js
@@ -0,0 +1,8 @@
+var burger = 300 ;
+
+
+if( burger >= 500){
+    console.log("free Coke")
+}else{
+    console.log("Coke: 30tk")
+}
