3110452c3f487794be1e8c5488cfde519afda222
Mintu Sikder <mintusikder15@gmail.com>
2025-02-08 10:11:44 +0600

    added index

---
+++ array/index.js
@@ -0,0 +1,12 @@
+const number = [1, 4, 6, 34, 53, 63, 64];
+console.log(number);
+console.log(number[2]);
+// get element value by index
+const forth = number[4];
+console.log(forth);
+//set or update value by index
+let abc = 34;
+abc = 99;
+
+number[1] = 999;
+console.log(number);
