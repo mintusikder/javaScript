f0634b7da4c7ba14a272caa003fde4f1a7f22ca1
Mintu Sikder <mintusikder15@gmail.com>
2025-02-08 13:10:10 +0600

    added isArray

---
+++ array/isArray.js
@@ -0,0 +1,13 @@
+const friends = ["balam", "colom", "robin", "mintu", "sajib"];
+
+const age = [];
+
+const food = "dim khete hobe";
+
+const price = 43;
+
+
+console.log(Array.isArray(friends))
+console.log(Array.isArray(age))
+console.log(Array.isArray(food))
+console.log(Array.isArray(price))
