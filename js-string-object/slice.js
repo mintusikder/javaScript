718955b29e8e4f9f74983e27ff934bf209d18328
Mintu Sikder <mintusikder15@gmail.com>
2025-02-09 20:09:58 +0600

    added slice

---
+++ js-string-object/slice.js
@@ -0,0 +1,17 @@
+const address = "sabujnagor"
+
+const part = address.slice(2,5)
+// console.log(part)
+
+const person = "I am a good person and hardworking"
+
+// console.log(person.split(' '))
+// console.log(person.split('a'))
+
+const friend = "rahim,karim,sakib,rakib,robin"
+// console.log(friend.split(","))
+// console.log(friend)
+
+const realFriend = ["robin","korim","sakib","sakil","sajib"]
+console.log(realFriend.join())
+console.log(realFriend.join("|"))
