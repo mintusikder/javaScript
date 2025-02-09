66aab2f50556b3934b9dca15104d0b56011ce706
Mintu Sikder <mintusikder15@gmail.com>
2025-02-09 21:08:35 +0600

    added object

---
+++ js-string-object/object.js
@@ -0,0 +1,15 @@
+const bottle ={
+    brand : "Apple",
+    price : 45,
+    color : "red",
+    isClear : true
+}
+console.log(bottle)
+
+const subject ={
+    name: "biology",
+    teacher : "Sajb",
+    examDate : "09 feb",
+    chapter : ['first', "second"]
+}
+console.log(subject)
