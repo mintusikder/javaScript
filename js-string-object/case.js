e0c834743b12488f78c27712601120d816913d9c
Mintu Sikder <mintusikder15@gmail.com>
2025-02-09 18:36:55 +0600

    added case

---
+++ js-string-object/case.js
@@ -0,0 +1,19 @@
+const subject = "math"
+const book = "Math"
+
+
+if(subject.toLocaleLowerCase() === book.toLocaleLowerCase()){
+    console.log("reading book")
+}else{
+    console.log("boi porte valo lage na")
+}
+
+
+const drink = "water"
+const  mojo = "water "
+
+if(drink.trim() === mojo.trim()){
+    console.log("ami pani khabo")
+}else{
+    console.log("ami kintu pai nai")
+}
