dd01863393726f96f32139422f2b44a2ed177dfa
Mintu Sikder <mintusikder15@gmail.com>
2025-02-09 20:19:30 +0600

    added concat

---
+++ js-string-object/concat.js
@@ -0,0 +1,12 @@
+const first = "Mintu";
+const last = "Sikder";
+
+// const fullName = first + last;
+const fullName = first + " "+last;
+// console.log(fullName);
+
+
+const fullName2 = first.concat(" ").concat(last)
+console.log(fullName2)
+
+console.log(last.includes("r"))
