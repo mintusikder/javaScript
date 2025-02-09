1910c311c1f93176493083a0fa5c20486da2365e
Mintu Sikder <mintusikder15@gmail.com>
2025-02-09 18:18:12 +0600

    added string

---
+++ js-string-object/string.js
@@ -0,0 +1,14 @@
+const number = [44,6,7,89,35]
+console.log(number)
+number[1] = 33
+console.log(number)
+
+
+
+const capital = "Dhaka"
+console.log(capital.length)
+console.log(capital[2])
+
+console.log(capital)
+capital[1] = "q"
+console.log(capital)
