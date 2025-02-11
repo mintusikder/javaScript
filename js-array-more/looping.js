5d28a103d2bb076f45ffc00d4ac2b531b995a724
Mintu Sikder <mintusikder15@gmail.com>
2025-02-11 08:22:13 +0600

    added looping

---
+++ js-array-more/looping.js
@@ -0,0 +1,23 @@
+const friends = ["Mintu", "Sajib", "Rakib", "Sakib"];
+
+for (const friend of friends) {
+  // console.log(friend)
+}
+
+for (let i = 0; i < friends.length; i++) {
+  console.log(i);
+  //   console.log(friends[i]);
+}
+
+const numbers = [34, 65, 76, 879, 9, 67, 879, 0, 687, 97, 97];
+
+for (let i = 0; i < numbers.length; i++) {
+  //   console.log(numbers[i]);
+}
+//while
+
+let i = 0;
+while (i < friends.length) {
+  console.log(friends[i]);
+  i++;
+}
