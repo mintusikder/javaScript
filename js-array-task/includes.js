1ac3a6dc004458fad7f37878cba64f0db33f3e10
Mintu Sikder <mintusikder15@gmail.com>
2025-02-12 19:07:25 +0600

    added includes

---
+++ js-array-task/includes.js
@@ -0,0 +1,9 @@
+const fruits = ["mango", "apple", "banana", "orange", "tomato"];
+
+const goodFood = fruits.includes("bananas")
+
+if(goodFood){
+    console.log("valo khabar khoya valo")
+}else{
+    console.log("valo khabar na khele sharir kharap kore")
+}
