2cfc256f55511253172beaf4dfcfb14c1989f15b
Mintu Sikder <mintusikder15@gmail.com>
2025-02-12 20:56:11 +0600

    added argument

---
+++ function/argument.js
@@ -0,0 +1,17 @@
+function double(number){
+    const doubled = number * 2
+    console.log(doubled,number)
+}
+
+// const money = 10;
+// double(money)
+
+//different 
+
+function different(num1, num2){
+    const diff = num1 - num2 
+    console.log(num1,num2, "different" ,diff)
+}
+const fatherAge = (55)
+const myAge = (21)
+different(fatherAge,myAge)
