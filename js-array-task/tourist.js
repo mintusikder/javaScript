6c0c868ff9a2190612f36da180995f97c0da2ce8
Mintu Sikder <mintusikder15@gmail.com>
2025-02-12 10:49:47 +0600

    tourist

---
+++ js-array-task/tourist.js
@@ -0,0 +1,10 @@
+const tourist = ["Kutakata","Sajak","bandorbon"]
+
+tourist.push("Sylat")
+console.log(tourist)
+
+tourist.push("Barisal","Khulna")
+console.log(tourist)
+
+tourist.pop()
+console.log(tourist)
