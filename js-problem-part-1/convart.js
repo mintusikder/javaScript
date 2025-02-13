bc3dcba632dc46d97bfa9687e0c928f2c8cbf242
Mintu Sikder <mintusikder15@gmail.com>
2025-02-13 22:01:22 +0600

    added convart

---
+++ js-problem-part-1/convart.js
@@ -0,0 +1,16 @@
+// 12 inch 1 feet
+
+function inchToFeet(inch) {
+  const feet = inch / 12;
+  return feet;
+}
+const mintuHight = inchToFeet(75)
+console.log(mintuHight)
+
+
+//mile to kilometer
+
+function mileTokilometer(mile){
+        const kilo = mile * 1.60934
+        return kilo
+}
