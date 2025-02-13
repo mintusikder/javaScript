dff0de15d33e8488fe41abb8061e60308f72de5b
Mintu Sikder <mintusikder15@gmail.com>
2025-02-13 18:21:55 +0600

    added function array

--- function/length.js
+++ function/length.js
@@ -1,5 +1,36 @@
-function chLength(str){
-    const st = str.length
-    console.log(str,st)
+function chLength(str) {
+  const st = str.length;
+  console.log(str, st);
+
+  if (st % 2 === 0) {
+    console.log("even size");
+    return true;
+  } else {
+    console.log("odd size");
+    return false;
+  }
 }
-chLength("dhaka")
+chLength("dhakas");
+
+// function double or triple
+
+function doubleOrTriple(number, doDouble) {
+  if (doDouble === true) {
+    const result = number * 2;
+    return result;
+  } else {
+    const result = number * 3;
+    return result;
+  }
+}
+
+console.log(doubleOrTriple(5, true));
+console.log(doubleOrTriple(5, false));
+
+function numberOfElement(number) {
+  const num = number.length;
+  return num;
+}
+numberOfElement([34, 65, 8, 90, 32, 24]);
+
+
