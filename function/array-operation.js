f0b70bb8ecf80432d96fc160243a94ff690e0731
Mintu Sikder <mintusikder15@gmail.com>
2025-02-13 18:45:09 +0600

    added array-operation

---
+++ function/array-operation.js
@@ -0,0 +1,13 @@
+function sumOfNumber(numbers) {
+  console.log(numbers);
+  let sum = 0;
+  for (const number of numbers) {
+    console.log(number);
+    sum = sum + number;
+ 
+  }
+  return sum;
+}
+const num = [34, 5, 76, 88, 32];
+const sum = sumOfNumber(num);
+console.log("Sum of number is", sum);
