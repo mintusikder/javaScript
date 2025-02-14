77a5b1a31e0a09108d7e91cd9b3244f58098890a
Mintu Sikder <mintusikder15@gmail.com>
2025-02-14 13:25:50 +0600

    added date

---
+++ js-problem-part-1/date.js
@@ -0,0 +1,10 @@
+const today = new Date()
+const date = new Date("2020-10-19")
+console.log(date.getMonth())
+console.log(date.getDate())
+
+const specificDate = new Date(2091,0,26)
+console.log(specificDate)
+specificDate.setMonth(10)
+console.log(specificDate)
+console.log(specificDate.toLocaleString("en-GB"))
