4066e44dad1f6cd091f3d6609376ffb1f255be19
Mintu Sikder <mintusikder15@gmail.com>
2025-02-14 12:00:22 +0600

    added math

---
+++ js-problem-part-1/math.js
@@ -0,0 +1,16 @@
+const min = Math.min(12,45,6,77,9)
+console.log(min)
+
+const max = Math.max(22,57,44,7878,46)
+console.log(max)
+
+console.log(Math.PI)
+console.log(Math.abs(20-5))
+console.log(Math.round(2.56))
+console.log(Math.floor(2.56))
+console.log(Math.ceil(2.56))
+
+console.log(Math.random()*10)
+
+const ran = Math.round(Math.random()*10)
+console.log(ran)
