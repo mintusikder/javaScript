68e5ffb0aa04a431e05f6063932a06f4fc93407b
Mintu Sikder <mintusikder15@gmail.com>
2025-02-15 09:52:26 +0600

    added max3

---
+++ js-problem-part-2/max3.js
@@ -0,0 +1,15 @@
+const jim = 66;
+const kim = 99;
+const luna = 100;
+
+if (jim > kim && jim > luna) {
+  console.log("Jim is big");
+} else if (kim > jim && kim > luna) {
+  console.log("kim is big");
+} else {
+  console.log("luna is big");
+}
+
+
+const math = Math.max(12,44,67,90)
+console.log(math)
